ba82b84b5cd7379451ae82546c21581f3b49da79
IR~SAGE <sibo.egide98@gmail.com>
2021-07-04 14:30:24 +0200

    Deciding The Winner

--- Js/GameView.js
+++ Js/GameView.js
@@ -4,12 +4,11 @@ export default class GameView {
   }
 
   updateBoard(game) {
-    // console.log("this is a board Within Game View");
-    // console.log(game.board);
-
+    
     for (let i = 0; i < game.board.length; i++) {
       const tile = document.querySelector(`.board-tile[data-index='${i}']`);
       tile.textContent = game.board[i];
     }
-  }
+    }
+    
 }
--- Js/main.js
+++ Js/main.js
@@ -5,17 +5,6 @@ let game = new Game();
 
 let gameView = new GameView();
 
-// console.log(game.board)
-// console.log("First Turn is ", game.turn)
-// game.nextTurn()
-// console.log("Second Turn is ", game.turn);
-// game.makeMove(3)
-// console.log(game.board)
-// game.nextTurn();
-// game.makeMove(8);
-// console.log(game.board);
-// gameView.updateBoard(game);
-
 let tiles = document.querySelectorAll(".board-tile");
 
 tiles.forEach((tile) => {
@@ -27,5 +16,4 @@ tiles.forEach((tile) => {
 function onTileClick(i) {
     game.makeMove(i);
     gameView.updateBoard(game);
-    game.nextTurn()
 }
