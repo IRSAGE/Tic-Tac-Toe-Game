e0c5d25d2b364854b55aee4cf39bbc9ec15c2cb3
IR~SAGE <sibo.egide98@gmail.com>
2021-07-04 15:58:54 +0200

    Presanting The Winner

--- Js/GameView.js
+++ Js/GameView.js
@@ -5,13 +5,20 @@ export default class GameView {
 
   updateBoard(game) {
     this.updateTurn(game);
+    const winningCombination = game.findWinningCombination();
+
     for (let i = 0; i < game.board.length; i++) {
       const tile = document.querySelector(`.board-tile[data-index='${i}']`);
+      tile.classList.remove("winner");
 
       let tileType = game.board[i] == "X" ? "tile-x" : "tile-o";
       tile.innerHTML = `<span class="${tileType}">${
         game.board[i] ? game.board[i] : ""
       }</span>`;
+
+      if (winningCombination && winningCombination.includes(i)) {
+        tile.classList.add("winner");
+      }
     }
   }
 
