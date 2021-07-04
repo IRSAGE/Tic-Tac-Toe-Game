05388d6733100802f9d2a05d9d574ed1c97e3096
IR~SAGE <sibo.egide98@gmail.com>
2021-07-04 12:39:13 +0200

    Adding Next Turn Functionality

---
+++ Js/Game.js
@@ -0,0 +1,15 @@
+export default class Game {
+  constructor() {
+    console.log("init game");
+    this.turn = "X";
+    this.board = new Array(9).fill(null);
+  }
+
+  nextTurn() {
+    if (this.turn == "X") {
+      this.turn = "O";
+    } else {
+      this.turn = "X";
+    }
+  }
+}
---
+++ Js/main.js
@@ -0,0 +1,5 @@
+import Game from "./Game.js";
+
+let game = new Game();
+console.log("hello");
+let gam = new Game();
